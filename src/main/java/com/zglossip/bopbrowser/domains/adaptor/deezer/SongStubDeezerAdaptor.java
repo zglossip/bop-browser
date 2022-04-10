b451324e9f59b893bea2fc57194265e02db82297
zglossip <zglossip@oreillyauto.com>
2022-04-10 08:24:58 -0500

    Finish domain tests

--- src/main/java/com/zglossip/bopbrowser/domains/adaptor/deezer/SongStubDeezerAdaptor.java
+++ src/main/java/com/zglossip/bopbrowser/domains/adaptor/deezer/SongStubDeezerAdaptor.java
@@ -4,11 +4,16 @@ import com.zglossip.bopbrowser.domains.SongStub;
 import com.zglossip.bopbrowser.domains.models.deezer.DeezerSong;
 
 import java.net.URI;
+import java.util.Objects;
 
 public class SongStubDeezerAdaptor extends DeezerSong implements SongStub {
 
   @Override
   public URI getAlbumArtUri() {
+    if (Objects.isNull(getAlbum())) {
+      return null;
+    }
+
     return getAlbum().getCover();
   }
 
---
+++ src/test/groovy/com/zglossip/bopbrowser/unit/adaptors/SongStubDeezerAdaptorSpec.groovy
@@ -0,0 +1,80 @@
+package com.zglossip.bopbrowser.unit.adaptors
+
+import com.zglossip.bopbrowser.domains.adaptor.deezer.SongStubDeezerAdaptor
+import com.zglossip.bopbrowser.domains.models.deezer.DeezerAlbum
+import spock.lang.Specification
+
+class SongStubDeezerAdaptorSpec extends Specification {
+
+  def 'Get album art URI'() {
+    given:
+    DeezerAlbum album = new DeezerAlbum(cover: albumArtUri)
+    songStub.setAlbum(album)
+
+    when:
+    URI result = songStub.getAlbumArtUri()
+
+    then:
+    result == albumArtUri
+
+    where:
+    songStub = new SongStubDeezerAdaptor()
+    albumArtUri = new URI('notreal.org')
+  }
+
+  def 'Get album art URI (null URI)'() {
+    given:
+    DeezerAlbum album = new DeezerAlbum()
+    songStub.setAlbum(album)
+
+    when:
+    URI result = songStub.getAlbumArtUri()
+
+    then:
+    result == null
+
+    where:
+    songStub = new SongStubDeezerAdaptor()
+  }
+
+  def 'Get album art URI (null album)'() {
+    when:
+    URI result = songStub.getAlbumArtUri()
+
+    then:
+    result == null
+
+    where:
+    songStub = new SongStubDeezerAdaptor()
+  }
+
+  def 'Get preview URI'() {
+    given:
+    songStub.setPreview(previewUri)
+
+    when:
+    URI result = songStub.getPreviewUri()
+
+    then:
+    result == previewUri
+
+    where:
+    songStub = new SongStubDeezerAdaptor()
+    previewUri = new URI('okay.ok')
+  }
+
+  def 'Get preview URI (null)'() {
+    given:
+    songStub.setPreview()
+
+    when:
+    URI result = songStub.getPreviewUri()
+
+    then:
+    result == null
+
+    where:
+    songStub = new SongStubDeezerAdaptor()
+  }
+
+}
