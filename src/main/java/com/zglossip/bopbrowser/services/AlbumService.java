7f4af9335d0a95b6425a31832d1b65047383c996
zglossip <zglossip@oreillyauto.com>
2022-04-16 14:16:05 -0500

    Add song list

--- src/main/java/com/zglossip/bopbrowser/services/AlbumService.java
+++ src/main/java/com/zglossip/bopbrowser/services/AlbumService.java
@@ -1,32 +1,52 @@
 package com.zglossip.bopbrowser.services;
 
+import com.zglossip.bopbrowser.clients.BasicClient;
 import com.zglossip.bopbrowser.clients.DeezerAlbumClient;
 import com.zglossip.bopbrowser.clients.DeezerSearchClient;
 import com.zglossip.bopbrowser.domains.Album;
 import com.zglossip.bopbrowser.domains.AlbumStub;
+import com.zglossip.bopbrowser.domains.adaptor.deezer.AlbumDeezerAdaptor;
 import com.zglossip.bopbrowser.domains.adaptor.deezer.AlbumStubDeezerAdaptor;
+import com.zglossip.bopbrowser.domains.adaptor.deezer.SongStubDeezerAdaptor;
+import com.zglossip.bopbrowser.domains.models.deezer.DeezerSongList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class AlbumService extends AbstractService<AlbumStub> {
 
   private final DeezerAlbumClient deezerAlbumClient;
   private final DeezerSearchClient deezerSearchClient;
+  private final BasicClient basicClient;
   private final GenreService genreService;
 
   @Autowired
   public AlbumService(final DeezerAlbumClient deezerAlbumClient, final DeezerSearchClient deezerSearchClient,
-                      final GenreService genreService) {
+                      final BasicClient basicClient, final GenreService genreService) {
     this.deezerAlbumClient = deezerAlbumClient;
     this.deezerSearchClient = deezerSearchClient;
+    this.basicClient = basicClient;
     this.genreService = genreService;
   }
 
   public Album getAlbumInfo(final int id) {
-    return deezerAlbumClient.getAlbumInfo(id);
+    final AlbumDeezerAdaptor album = deezerAlbumClient.getAlbumInfo(id);
+
+    if (Objects.isNull(album)) {
+      return null;
+    }
+
+    final DeezerSongList songList = basicClient.getRequest(album.getTracklist(), DeezerSongList.class);
+
+    if (Objects.nonNull(songList) && Objects.nonNull(songList.getData())) {
+      album.setSongList(songList.getData().stream().map(SongStubDeezerAdaptor::clone).collect(Collectors.toList()));
+    }
+
+    return album;
   }
 
   @Override
--- src/test/groovy/com/zglossip/bopbrowser/unit/services/AlbumServiceSpec.groovy
+++ src/test/groovy/com/zglossip/bopbrowser/unit/services/AlbumServiceSpec.groovy
@@ -1,11 +1,14 @@
 package com.zglossip.bopbrowser.unit.services
 
+import com.zglossip.bopbrowser.clients.BasicClient
 import com.zglossip.bopbrowser.clients.DeezerAlbumClient
 import com.zglossip.bopbrowser.clients.DeezerSearchClient
 import com.zglossip.bopbrowser.domains.Album
 import com.zglossip.bopbrowser.domains.AlbumStub
 import com.zglossip.bopbrowser.domains.adaptor.deezer.AlbumDeezerAdaptor
 import com.zglossip.bopbrowser.domains.adaptor.deezer.AlbumStubDeezerAdaptor
+import com.zglossip.bopbrowser.domains.models.deezer.DeezerSong
+import com.zglossip.bopbrowser.domains.models.deezer.DeezerSongList
 import com.zglossip.bopbrowser.services.AlbumService
 import com.zglossip.bopbrowser.services.GenreService
 import spock.lang.Specification
@@ -18,16 +21,37 @@ class AlbumServiceSpec extends Specification {
 
   DeezerAlbumClient deezerAlbumClient
   DeezerSearchClient deezerSearchClient
+  BasicClient basicClient
   GenreService genreService
 
   def setup() {
     deezerAlbumClient = Mock(DeezerAlbumClient)
     deezerSearchClient = Mock(DeezerSearchClient)
+    basicClient = Mock(BasicClient)
     genreService = Mock(GenreService)
-    albumService = new AlbumService(deezerAlbumClient, deezerSearchClient, genreService)
+    albumService = new AlbumService(deezerAlbumClient, deezerSearchClient, basicClient, genreService)
   }
 
   def 'Get album info'() {
+    given:
+    def expected = new AlbumDeezerAdaptor(id: id, tracklist: tracklist)
+
+    when:
+    Album result = albumService.getAlbumInfo(id)
+
+    then:
+    1 * deezerAlbumClient.getAlbumInfo(id) >> expected
+    1 * basicClient.getRequest(tracklist, DeezerSongList.class) >> new DeezerSongList(data: songList)
+    result.equals(expected)
+    result.getSongList().equals(songList)
+
+    where:
+    id = 123
+    tracklist = new URI("test")
+    songList = [new DeezerSong(id: 1), new DeezerSong(id: 3)]
+  }
+
+  def 'Get album info (no tracklist)'() {
     given:
     def expected = new AlbumDeezerAdaptor(id: id)
 
@@ -36,6 +60,7 @@ class AlbumServiceSpec extends Specification {
 
     then:
     1 * deezerAlbumClient.getAlbumInfo(id) >> expected
+    0 * basicClient.getRequest(_)
     result.equals(expected)
 
     where:
