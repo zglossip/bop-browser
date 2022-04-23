58529465823d1128b49ef07e303b43d31fe08e6c
zglossip <zglossip@oreillyauto.com>
2022-04-23 14:38:04 -0500

    Add Album page

--- frontend/src/views/Album.vue
+++ frontend/src/views/Album.vue
@@ -1,5 +1,93 @@
 <template>
   <div>
-    <!--    TODO Fill out-->
+    <navbar class="mb-3" />
+    <div class="container">
+      <div v-if="isLoading" class="row">
+        <div class="col">Loading...</div>
+      </div>
+      <div v-else class="row mb-3">
+        <div class="col">
+          <a href="#" @click="navigateToArtist(album.artistId)">
+            &#8592; {{ album.artistName }}
+          </a>
+        </div>
+      </div>
+      <div class="row mb-2">
+        <div class="col">
+          <album-header
+            :duration="duration"
+            :genre-list="album.genreList"
+            :name="album.title"
+            :picture-uri="album.pictureUri"
+            :release-date="album.releaseDate"
+          />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <tracklist :album-id="album.id" :song-list="album.songList" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import AlbumHeader from "@/components/album/AlbumHeader.vue";
+import Tracklist from "@/components/album/Tracklist.vue";
+import { computed, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import axios from "axios";
+import { API_URIS } from "@/util/constants";
+import { Duration } from "luxon";
+
+export default {
+  components: { AlbumHeader, Tracklist, Navbar },
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+
+    const album = ref({});
+    const isLoading = ref(true);
+    const navigateToArtist = (artistId) => {
+      router.push({
+        name: "Artist",
+        params: { id: artistId },
+      });
+    };
+
+    const duration = computed(() => {
+      const albumLength = album.value.songList.reduce(
+        (length, song) => length + song.duration,
+        0
+      );
+      //If duration longer or equal to an hour
+      if (albumLength >= 3600) {
+        return Duration.fromMillis(albumLength * 1000).toFormat(
+          "h 'hour(s), 'm' minute(s)'"
+        );
+      }
+      return Duration.fromMillis(albumLength * 1000).toFormat(
+        "m 'minute(s), ' s 'second(s)'"
+      );
+    });
+
+    axios
+      .get(API_URIS.getAlbum(route.params.id))
+      .then((response) => {
+        if (response) {
+          album.value = response.data;
+        }
+      })
+      .catch(() => {
+        //TODO Handle error
+      })
+      .finally(() => {
+        isLoading.value = false;
+      });
+
+    return { album, isLoading, navigateToArtist, duration };
+  },
+};
+</script>
